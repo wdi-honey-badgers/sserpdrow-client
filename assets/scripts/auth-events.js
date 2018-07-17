ebf94c270a6800ef1ca7f8ff76f3d64e8937e9c5
punzie <c_a_mcginty@yahoo.com>
2018-07-17 15:05:56 -0400

    User auth for api and ui
    Add api.js and ui.js actions for user auth
    Add heroku deployed url to config.js

--- assets/scripts/auth-events.js
+++ assets/scripts/auth-events.js
@@ -1,7 +1,7 @@
 'use strict'
 
-// const authApi = require('./api.js')
-// const authUi = require('./ui.js')
+const authApi = require('./api.js')
+const authUi = require('./ui.js')
 // const events = require('./events.js')
 const getFormFields = require('../../lib/get-form-fields')
 
@@ -9,28 +9,28 @@ const onSignUp = function (event) {
   event.preventDefault()
   console.log('you pushed a button!')
   const data = getFormFields(event.target)
-  console.log('commander lt. data is ', data)
-//   authApi.signUp(data)
-//     .then(authUi.signUpSuccess)
-//     .catch(authUi.error)
+  console.log('Lt. Commander Data is ', data)
+  authApi.signUp(data)
+    .then(authUi.signUpSuccess)
+    .catch(authUi.error)
 }
 
 const onSignIn = function (event) {
   event.preventDefault()
   console.log('you pushed a different button!')
   const data = getFormFields(event.target)
-  console.log('commander lt. data is ', data)
-//   authApi.signIn(data)
-//     .then(authUi.signInSuccess)
-//     .then(events.onGetCookies)
-//     .catch(authUi.error)
+  console.log('Lt. Commander Data is ', data)
+  authApi.signIn(data)
+    .then(authUi.signInSuccess)
+    // .then(events.onGetCookies)
+    .catch(authUi.error)
 }
 
 const onChangePassword = function (event) {
   event.preventDefault()
   console.log('you pushed the password button!')
   const data = getFormFields(event.target)
-  console.log('commader lt data is ', data)
+  console.log('Lt. Commander Data is ', data)
 //   authApi.changePassword(data)
 //     .then(authUi.changePasswordSuccess)
 //     .catch(authUi.error)
