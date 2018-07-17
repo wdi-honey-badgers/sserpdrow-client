139442564a4ee2f185265c9553df35da16eeda9f
punzie <c_a_mcginty@yahoo.com>
2018-07-17 13:31:47 -0400

    JQuery handlers
    Add jQuery handlers to sign-up, sing-in, sign-out, and change password
    buttons.
    Add auth-events.js. Currently just console logs button presses and data.

---
+++ assets/scripts/auth-events.js
@@ -0,0 +1,52 @@
+'use strict'
+
+// const authApi = require('./api.js')
+// const authUi = require('./ui.js')
+// const events = require('./events.js')
+const getFormFields = require('../../lib/get-form-fields')
+
+const onSignUp = function (event) {
+  event.preventDefault()
+  console.log('you pushed a button!')
+  const data = getFormFields(event.target)
+  console.log('commander lt. data is ', data)
+//   authApi.signUp(data)
+//     .then(authUi.signUpSuccess)
+//     .catch(authUi.error)
+}
+
+const onSignIn = function (event) {
+  event.preventDefault()
+  console.log('you pushed a different button!')
+  const data = getFormFields(event.target)
+  console.log('commander lt. data is ', data)
+//   authApi.signIn(data)
+//     .then(authUi.signInSuccess)
+//     .then(events.onGetCookies)
+//     .catch(authUi.error)
+}
+
+const onChangePassword = function (event) {
+  event.preventDefault()
+  console.log('you pushed the password button!')
+  const data = getFormFields(event.target)
+  console.log('commader lt data is ', data)
+//   authApi.changePassword(data)
+//     .then(authUi.changePasswordSuccess)
+//     .catch(authUi.error)
+}
+
+const onSignOut = function (event) {
+  event.preventDefault()
+  console.log("nooooo don't leave meeeee :'(")
+//   authApi.signOut()
+//     .then(authUi.signOutSuccess)
+//     .catch(authUi.error)
+}
+
+module.exports = {
+  onSignUp,
+  onSignIn,
+  onChangePassword,
+  onSignOut
+}
